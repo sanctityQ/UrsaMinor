655cf3cf3c4628776b46429faad234ca9ddb34f6
zhouchangbao <zhouchangbao@tftiancai.com>
2018-05-30 11:56:33 +0800

    注册去掉发送优惠券及绑定邀请关系逻辑
    Change-Id: I8fb2b6e87f954b68ce69a519bf87462084b9bd63

--- Polaris/app/controller/api.js
+++ Polaris/app/controller/api.js
@@ -6,8 +6,6 @@
  */
 var passportModel = require('../model/passport.js');
 var captcha2Model = require('../model/captcha2.js');
-var userModel = require('../model/user.js');
-var interactModel = require('../model/interact.js');
 var tclog = require('../libs/tclog.js');
 var tokenModel = require('../model/token.js');
 var _ = require('underscore');
@@ -67,24 +65,6 @@ module.exports = {
     };
     tclog.notice({api:'/api/register', registerInfo: _.omit(registerInfo, 'password')});
     try {
-      //TODO 是否要放在这里做? 校验邀请码
-      var inviteCode = postBody.inviteCode;
-      if(inviteCode) {
-        //邀请码不区分大小写
-        inviteCode = inviteCode.toUpperCase();
-        try{
-          //如果填写邀请码,验证邀请码是否正确
-          var inviteInfo = yield interactModel.findInviteInfoByUserKey(inviteCode);
-          tclog.debug({inviteInfo : inviteInfo});
-        } catch(err) {
-          if(err.err_code == 10001) {
-            //interact服务异常,不能影响注册
-            tclog.error({msg : "findInviteInfoByUserKey err"})
-          } else {
-            throw err;
-          }
-        }
-      }
       //短信验证码是否正确
       var biz_type = captcha2Model.BIZ_TYPE.REGISTER;
       var validObj = {biz_type:biz_type, captcha:smsCaptcha, mobile:registerInfo.mobile};
@@ -100,17 +80,6 @@ module.exports = {
       var passportUser = yield passportModel.login(loginInfo);
       var tokenNo = yield tokenModel.putToken(loginInfo, passportUser);
       var result = {access_token:tokenNo, user:passportUser,msg:'登录成功'};
-      //初始化p2p用户
-      userModel.findUserByPassportUser(passportUser).then(function(user) {
-        if(inviteCode) { //如果填写了邀请码
-          tclog.notice({msg:"triggerInteract register", user:user.id, inviteCode:inviteCode});
-          interactModel.triggerInteract(0, user.id, inviteCode, user.id);
-        }
-        tclog.notice({msg:"autoSendCoupon register", user:user.id});
-        interactModel.autoSendCoupon(user.id);
-      }).catch(function(err) {
-        tclog.error({msg:"findUserByPassportUser error", err:err});
-      });
       yield this.api(result);
     } catch (err) {
       tclog.warn({api:'/api/register', traceNo:traceNo, err:err});
